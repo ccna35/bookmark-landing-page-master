e0f6ffbd94b62a851ec36f2dafa58d0581728cb7
ccna35 <shawkykhalil@gmail.com>
2021-12-26 09:00:45 +0200

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 const hiddenMobileMenu = document.querySelector("#navbarNavAltMarkup");
 const burgerIcon = document.querySelector(".navbar-toggler-icon");
+const closeIcon = document.querySelector(".navbar-toggler-icon-close");
+const myNavLogo = document.querySelector(".myNavLogo");
 
-icon.addEventListener;
+console.log(hiddenMobileMenu.classList.contains("show"));
+
+let active = false;
+
+// if (hiddenMobileMenu.classList.contains("show")) {
+//   closeIcon.classList.add("active");
+//   myNavLogo.classList.add("active");
+// }
+
+burgerIcon.addEventListener("click", () => {
+  closeIcon.classList.add("active");
+  myNavLogo.classList.add("active");
+  burgerIcon.style.display = "none";
+});
+
+closeIcon.addEventListener("click", () => {
+  closeIcon.classList.remove("active");
+  myNavLogo.classList.remove("active");
+  hiddenMobileMenu.classList.remove("show");
+  burgerIcon.style.display = "block";
+});
